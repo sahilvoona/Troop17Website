efa556ad329c98e0dea788f7beeaaecd66ec3a4e
Ucef27 <pharaoh14@verizon.net>
2020-10-19 21:41:05 -0400

    added scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,4 +186,16 @@
     items: 1
   });
 
+  /*--/ Star Typed /--*/
+  if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+    var typed = new Typed('.text-slider', {
+      strings: typed_strings.split(','),
+      typeSpeed: 80,
+      loop: true,
+      backDelay: 1100,
+      backSpeed: 30
+    });
+  }
+
 })(jQuery);
